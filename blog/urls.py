ec6229860ecd04f778987618563bece00b84d68b
Moussa-codeur <correamoussa469@gmail.com>
2025-05-18 15:49:49 +0200

    Création de la page articles_par_categorie, articles_par_tag.html et base.html

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.accueil, name='accueil'),
     path('article/<slug:slug>/', views.article_detail, name='article_detail'),
-    
+    path('categorie/<int:id>/', views.articles_par_categorie, name='articles_par_categorie'),
+    path('tag/<int:id>/', views.articles_par_tag, name='articles_par_tag'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from .models import Article
+from .models import Article, Tag
 
 # Create your views here.
 def accueil(request):
@@ -10,3 +10,19 @@ def article_detail(request, slug):
     article = get_object_or_404(Article, slug=slug, statut='publie')
     commentaires = article.commentaires.filter(approuve=True)
     return render(request, 'blog/article_detail.html', {'article': article, 'commentaires': commentaires})
+
+def articles_par_categorie(request, id):
+    categorie = get_object_or_404(id=id)
+    articles = Article.objects.filter(categorie=categorie, statut='publie').order_by('-date_publication')
+    return render(request, 'blog/articles_par_categorie.html', {
+        'categorie': categorie,
+        'articles': articles
+    })
+    
+def articles_par_tag(request, id):
+    tag = get_object_or_404(Tag, id=id)
+    articles = Article.objects.filter(tags=tag, statut='publie').order_by('-date_publication')
+    return render(request, 'blog/articles_par_tag.html', {
+        'tag': tag,
+        'articles': articles
+    })
