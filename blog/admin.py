df1ca8fec97c4651160d55d05ad6e6c59e4a1f3c
Moussa-codeur <correamoussa469@gmail.com>
2025-05-18 13:43:42 +0200

    Mise en place du model, de l'admin, de apps et la création d'un fichier signals

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Article, Categorie, Tag, Commentaire, ProfilAuteur
 
-# Register your models here.
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'auteur', 'date_publication', 'statut')
+    list_filter = ('statut', 'categorie', 'tags')
+    search_fields = ('titre', 'contenu')
+    prepopulated_fields = {'slug': ('titre',)}
+
+@admin.register(Commentaire)
+class CommentaireAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'article', 'date', 'approuve')
+    list_filter = ('approuve', 'date')
+    search_fields = ('nom', 'message')
+
+@admin.register(ProfilAuteur)
+class ProfilAuteurAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    search_fields = ('user__username',)
+
+admin.site.register(Categorie)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,60 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.utils.text import slugify
 # Create your models here.
+class Categorie(models.Model):
+    nom = models.CharField(max_length=100, unique=True)
+    
+    def __str__(self):
+        return self.nom
+    
+class Tag(models.Model):
+    nom = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.nom
+    
+class Article(models.Model):
+    STATUT_CHOICES = (
+        ('brouillon', 'Brouillon'),
+        ('publie', 'Publié'),
+    )
+    
+    titre = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    contenu = models.TextField()
+    image = models.ImageField(upload_to='articles/', blank=True, null=True)
+    auteur = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_publication = models.DateTimeField(auto_now_add=True)
+    categorie = models.ForeignKey(Categorie, on_delete=models.SET_NULL, null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    statut = models.CharField(max_length=10, choices=STATUT_CHOICES, default='brouillon')
+
+    def __str__(self):
+        return self.titre
+
+    def save(self, *args, **kwargs):
+        if not self.slug: 
+            self.slug = slugify(self.titre)
+        super().save(*args, **kwargs)
+        
+
+class Commentaire(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='commentaires')
+    nom = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    approuve = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.nom} - {self.article.titre}"
+    
+    
+class ProfilAuteur(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    photo = models.ImageField(upload_to='profils/', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
