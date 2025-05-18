cf761d6aace908e391fe59c48402b0757ab0398d
Moussa-codeur <correamoussa469@gmail.com>
2025-05-18 15:16:54 +0200

    Crétion de la page d'accueil avec la liste des articles récent.Création d'undossier templates pout y mettre mes fichiers html

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from .models import Article, Categorie, Tag, Commentaire, ProfilAuteur
 
-@admin.register(Article)
+@admin.register(Article) #La ligne `@admin.register(Commentaire)` est un décorateur fourni par Django pour simplifier l’enregistrement d’un modèle dans l’interface d’administration.
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('titre', 'auteur', 'date_publication', 'statut')
     list_filter = ('statut', 'categorie', 'tags')
     search_fields = ('titre', 'contenu')
-    prepopulated_fields = {'slug': ('titre',)}
+    prepopulated_fields = {'slug': ('titre',)} #est un dictionnaire qui indique à Django de remplir automatiquement le champ slug à partir du champ titre. 
 
 @admin.register(Commentaire)
 class CommentaireAdmin(admin.ModelAdmin):
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+def accueil(request):
+    articles = Article.objects.filter(statut='publié').order_by('-date_publication')
+    return render(request, 'blog/accueil.html', { 'articles':articles})
